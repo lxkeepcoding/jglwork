51c36a1e36fe8accaadd9696e7d041f99244dd38
lxkeepcoding <88451719+lxkeepcoding@users.noreply.github.com>
2021-11-14 19:35:24 +0800

    字符串替换利用string

--- test.cpp
+++ test.cpp
@@ -1,60 +1,74 @@
 #define _CRT_SECURE_NO_WARNINGS   1
-#include"LinkList.cpp"
-#include"SeqList.cpp"
-#include<iostream>
-using namespace std;
 
-void menu()
+#include<iostream>
+#include<string>
+using namespace std;
+string Replace(string s1, string s2, string s3, int pos)
 {
-	cout << "|--------------------------------------------|" << endl;
-	cout << "|欢迎来到两个有序线性表的合并，请选择实现方式|" << endl;
-	cout << "|******************  0.退出    **************|" << endl;
-	cout << "|******************  1.顺序表  **************|" << endl;
-	cout << "|******************  2.链表    **************|" << endl;
-	cout << "|--------------------------------------------|" << endl;
+	string s4;
+	s4 = s1.substr(0, pos) + s3 + s1.substr((pos + s2.length()), s1.length() - 1);
+	return s4;
 }
+int* getNext(string p)
+{
+	int* next = new int[p.length()];
+	next[0] = -1;         
+	int j = 0;
+	int k = -1;
+	while (j < (int)p.length() - 1)
+	{
+		if (k == -1 || p[j] == p[k])
+		{
+			j++;
+			k++;
+			next[j] = k;
+		}
+		else
+		{
+			k = next[k];
+		}
+	}
+	return next;
+}
+
+int KMP(string T, string p)
+{
+	int i = 0;
+	int j = 0;
+	int* next = getNext(T);
+	while (i < (int)T.length() && j < (int)p.length())
+	{
+		if (j == -1 || T[i] == p[j])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			j = next[j];
+		}
+	}
+	if (j == (int)p.length())
+	{
+		return i - j;
+	}
+	return -1;
+}
+
 
 int main()
 {
-	int *a;
-	int n;
-	cin >> n;
-	a = new int[n];
-	for (int i = 0; i < n; i++)
+	string str1, str2, str3;
+	cin >> str1;
+	cin >> str2;
+	cin >> str3;
+	int pos = 0;
+	pos = KMP(str1, str2);
+	while (pos != -1)
 	{
-		cin >> a[i];
+		str1=Replace(str1, str2, str3, pos);
+		pos = KMP(str1, str2);
 	}
-	int *b;
-	int m;
-	cin >> m;
-	b = new int[m];
-	for (int i = 0; i < m; i++)
-	{
-		cin >> b[i];
-	}
-	SeqList<int, 20>Seq1(a, n);
-	SeqList<int, 20>Seq2(b, m);
-	LinkList<int> Link1(a, n);
-	LinkList<int>Link2(b, m);
-	int input = 0;
-	do
-	{
-		menu();
-		cin >> input;
-		switch (input)
-		{
-		case 1:
-			SeqListMerge(Seq1,Seq2);
-			break;
-		case 2:
-			LinkListMerge(Link1, Link2);
-			break;
-		case 0:
-			cout << "感谢您的使用，程序退出" << endl;
-			break;
-		default:
-			cout << "输入错误，请重新输入，或者，您可以选择0来终止程序" << endl;
-		}
-	} while (input);
+	cout << str1 << endl;
 	return 0;
 }
